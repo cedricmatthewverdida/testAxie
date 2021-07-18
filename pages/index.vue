44918a157a179c9df71ab6fdbd0da2f95b229efe
Cedric Matthew Verdida <chryhesmicazumic@gmail.com>
2021-07-18 12:47:24 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -498,7 +498,8 @@
                         "query":"query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {\n  axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {\n    total\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  id\n  name\n  stage\n  class\n  breedCount\n  image\n  title\n  battleInfo {\n    banned\n    __typename\n  }\n  auction {\n    currentPrice\n    currentPriceUSD\n    __typename\n  }\n  parts {\n    id\n    name\n    class\n    type\n    specialGenes\n    __typename\n  }\n  __typename\n}\n"
                     },
                     headers: {
-                        'Content-Type': 'text/plain;charset=utf-8',
+                        'Accept'       : 'application/json',
+                        'Content-Type' : 'application/json',
                     },
 
         
