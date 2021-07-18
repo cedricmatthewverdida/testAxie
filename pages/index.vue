f4accbd6d3a007e11064754cff0ed7cc3bdfdd6e
Cedric Matthew Verdida <chryhesmicazumic@gmail.com>
2021-07-18 12:25:35 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -456,43 +456,52 @@
             
             if(this.Clear == false){
             this.axie = []
-            const response = await this.$axios.post('/axie',
-                {
-                "operationName":"GetAxieBriefList",
-                "variables":{
-                    "from":this.currentPage,
-                    "size":24,
-                    "sort":this.priceSearch,
-                    "auctionType":this.auctionSearch,
-                    "owner":null,
-                    "criteria":{
-                        "region":null,
-                        "parts":null,
-                        "bodyShapes":null,
-                        "classes":this.selected,
-                        "stages":this.selected_stage,
-                        "numMystic":[
-                            this.mystic
-                        ],
-                        "pureness":null,
-                        "title":null,
-                        "breedable":null,
-                        "breedCount":null,
-                        "hp":[
-                            
-                        ],
-                        "skill":[
-                            
-                        ],
-                        "speed":[
-                            
-                        ],
-                        "morale":[
-                            
-                        ]
-                    }
-                },
-                "query":"query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {\n  axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {\n    total\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  id\n  name\n  stage\n  class\n  breedCount\n  image\n  title\n  battleInfo {\n    banned\n    __typename\n  }\n  auction {\n    currentPrice\n    currentPriceUSD\n    __typename\n  }\n  parts {\n    id\n    name\n    class\n    type\n    specialGenes\n    __typename\n  }\n  __typename\n}\n"
+            const response = await this.$axios({
+                method: 'post',
+
+                url:'/axie',
+                data:{
+                        "operationName":"GetAxieBriefList",
+                        "variables":{
+                            "from":this.currentPage,
+                            "size":24,
+                            "sort":this.priceSearch,
+                            "auctionType":this.auctionSearch,
+                            "owner":null,
+                            "criteria":{
+                                "region":null,
+                                "parts":null,
+                                "bodyShapes":null,
+                                "classes":this.selected,
+                                "stages":this.selected_stage,
+                                "numMystic":[
+                                    this.mystic
+                                ],
+                                "pureness":null,
+                                "title":null,
+                                "breedable":null,
+                                "breedCount":null,
+                                "hp":[
+                                    
+                                ],
+                                "skill":[
+                                    
+                                ],
+                                "speed":[
+                                    
+                                ],
+                                "morale":[
+                                    
+                                ]
+                            }
+                        },
+                        "query":"query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {\n  axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {\n    total\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  id\n  name\n  stage\n  class\n  breedCount\n  image\n  title\n  battleInfo {\n    banned\n    __typename\n  }\n  auction {\n    currentPrice\n    currentPriceUSD\n    __typename\n  }\n  parts {\n    id\n    name\n    class\n    type\n    specialGenes\n    __typename\n  }\n  __typename\n}\n"
+                    },
+                    headers: {
+                        'Content-Type': 'text/plain;charset=utf-8',
+                    },
+
+        
                 }
             );
             const store = await response.data;
